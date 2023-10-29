31ed03d7bf1d2e54f7be456c430e4c1ad7e7797c
Chukky-27 <cartusky4eva88@gmail.com>
2023-10-29 13:23:15 +0100

    29-10

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            _bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            _bloggieDbContext.Tags.Add(tag);
+            _bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            //use dbcontext to read the tags
+            var tags =_bloggieDbContext.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
